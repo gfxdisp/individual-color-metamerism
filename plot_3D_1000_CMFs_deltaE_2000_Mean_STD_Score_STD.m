c2826472881d5f02c26ca41fe4020ae21039ed62
unknown <19307140030@fudan.edu.cn>
2023-10-31 15:28:57 +0000

    2023-10-31

--- plot_3D_1000_CMFs_deltaE_2000_Mean_STD_Score_STD.m
+++ plot_3D_1000_CMFs_deltaE_2000_Mean_STD_Score_STD.m
@@ -7,6 +7,14 @@ AllData(4621:4752,:) = [];
 ciexyz31_1 = table2array(readtable('ObserverMetamerism\ObserverMetamerism\Data\AuxData/ciexyz31_1.csv')); % 471*4
 Obs_1000_CMF_struct = load("ObserverMetamerism\ObserverMetamerism\Data\AuxData/IndividualObs_2deg.mat");
 Obs_1000_CMFs = Obs_1000_CMF_struct.xyz_CMFs;
+original_wavelengths = 390:5:780;
+desired_wavelengths = 380:1:780;
+Obs_1000_CMFs_1nm = zeros(length(desired_wavelengths), 3, 1000);
+for i = 1:1000
+    for j = 1:3
+        Obs_1000_CMFs_1nm(:, j, i) = interp1(original_wavelengths, Obs_1000_CMFs(:, j, i), desired_wavelengths, 'linear', 'extrap');
+    end
+end
 display_1_spd = table2array(readtable('ObserverMetamerism\ObserverMetamerism\Data\Spectra/C2_Spectra.csv'));
 display_2_spd = table2array(readtable('ObserverMetamerism\ObserverMetamerism\Data\Spectra/X310_Spectra.csv'));
 display_3_spd = table2array(readtable('ObserverMetamerism\ObserverMetamerism\Data\Spectra/Projector_Spectra.csv'));
@@ -32,16 +40,16 @@ for display_pattern_index = 1:6
         E_set = [];
         for obs = 1:num_obs
             [X1,X2,Y1,Y2,Z1,Z2] = deal(0,0,0,0,0,0);
-            for lamda = 390:5:780
-                X1 = X1 + display_spd_1(color_index,lamda-379) * Obs_1000_CMFs((lamda-385)/5,1,obs) * 5;
-                X2 = X2 + display_spd_2(color_index,lamda-379) * Obs_1000_CMFs((lamda-385)/5,1,obs) * 5;
-                Y1 = Y1 + display_spd_1(color_index,lamda-379) * Obs_1000_CMFs((lamda-385)/5,2,obs) * 5;
-                Y2 = Y2 + display_spd_2(color_index,lamda-379) * Obs_1000_CMFs((lamda-385)/5,2,obs) * 5;
-                Z1 = Z1 + display_spd_1(color_index,lamda-379) * Obs_1000_CMFs((lamda-385)/5,3,obs) * 5;
-                Z2 = Z2 + display_spd_2(color_index,lamda-379) * Obs_1000_CMFs((lamda-385)/5,3,obs) * 5;
+            for lamda = 380:1:780
+                X1 = X1 + display_spd_1(color_index,lamda-379) * Obs_1000_CMFs_1nm(lamda-379,1,obs);
+                X2 = X2 + display_spd_2(color_index,lamda-379) * Obs_1000_CMFs_1nm(lamda-379,1,obs);
+                Y1 = Y1 + display_spd_1(color_index,lamda-379) * Obs_1000_CMFs_1nm(lamda-379,2,obs);
+                Y2 = Y2 + display_spd_2(color_index,lamda-379) * Obs_1000_CMFs_1nm(lamda-379,2,obs);
+                Z1 = Z1 + display_spd_1(color_index,lamda-379) * Obs_1000_CMFs_1nm(lamda-379,3,obs);
+                Z2 = Z2 + display_spd_2(color_index,lamda-379) * Obs_1000_CMFs_1nm(lamda-379,3,obs);
             end
-            XYZ1 = [X1,Y1,Z1];
-            XYZ2 = [X2,Y2,Z2];
+            XYZ1 = [X1*683,Y1*683,Z1*683];
+            XYZ2 = [X2*683,Y2*683,Z2*683];
             E = CIE2000deltaE_XYZ(XYZ1,XYZ2,whitepoint( 'd65' )*100);
             E_set = [E_set,E];
         end
